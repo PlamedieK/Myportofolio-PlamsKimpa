349775bddd14ec259c65dc1e780fa6068d4afa44
PlamedieK <pkimpambudi@gmail.com>
2025-11-06 22:12:26 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",         // ton fichier HTML principal
+    "./src/**/*.{html,js}"  // tous les fichiers HTML et JS dans src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
